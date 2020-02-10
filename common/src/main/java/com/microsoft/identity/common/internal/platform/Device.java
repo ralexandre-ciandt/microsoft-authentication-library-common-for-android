ef14fbbbc9f9976b9cca43465a3b14842f971e48
Brian Melton-Grace <brianmel@microsoft.com>
2020-02-10 12:36:07 -0800

    Update version codes for release candidate (1.0.15-rc1)

--- common/src/main/java/com/microsoft/identity/common/internal/platform/Device.java
+++ common/src/main/java/com/microsoft/identity/common/internal/platform/Device.java
@@ -73,7 +73,7 @@ public final class Device {
         /**
          * The String representing the sdk platform version.
          */
-        public static final String PRODUCT_VERSION = "1.3.0";
+        public static final String PRODUCT_VERSION = "1.3.0-rc1";
 
         /**
          * The String representing the sdk version.
